de4332070363fbbbf39ca528e0c85ce703739b2d
phiona-b3 <phiona030303@gmail.com>
2023-02-02 13:29:28 +0300

    Task 0

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+'''lockboxes'''
+
+
+def canUnlockAll(boxes):
+    '''a method to determine if all boxes can be opened'''
+    unlocked = [0]
+    for box_id, box in enumerate(boxes):
+        if not box:
+            continue
+        for key in box:
+            if key < len(boxes) and key not in unlocked and key != box_id:
+                unlocked.append(key)
+    if len(unlocked) == len(boxes):
+        return True
+    return False
