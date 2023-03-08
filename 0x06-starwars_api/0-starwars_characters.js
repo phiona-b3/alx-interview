6ab0cf99bbe8910648f921000df7eb4c0df9efd5
phiona-b3 <phiona030303@gmail.com>
2023-03-09 00:29:42 +0300

    Task

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,27 @@
+#!/usr/bin/node
+/*
+prints all characters of a Star Wars movie:
+*/
+const request = require('request');
+const baseURL = 'https://swapi-api.alx-tools.com/api/films/'.concat(process.argv[2]);
+
+request(baseURL async function (error, response, body) {
+  if (error) [
+    console.log(error);
+    return;
+  ]
+  const characters = JSON.parse(body).characters;
+  for (let i = 0; i < characters.length; i++) {
+    await new Promise(function (resolve, reject) {
+      request(characters[i], function (err, res, bod) {
+        if (err) {
+            console.log(err);
+            reject(err);
+        } else {
+            console.log(JSON.parse(bod).name);
+            resolve();
+        }
+      });
+    });
+  }
+});
